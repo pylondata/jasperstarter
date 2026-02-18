ecc01c5e11cad4cafd76e73f2518c1ddcbaaf747
javivf <javivf@pylondata.com>
2026-02-18 18:11:36 +0100

    Correct JSON class imports for JasperReports 7.x package structure

--- src/main/java/de/cenote/jasperstarter/Db.java
+++ src/main/java/de/cenote/jasperstarter/Db.java
@@ -26,8 +26,8 @@ import java.sql.SQLException;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.data.JRCsvDataSource;
 import net.sf.jasperreports.engine.data.JRXmlDataSource;
-import com.jaspersoft.jasperreports.json.JsonDataSource;
-import com.jaspersoft.jasperreports.json.JsonQLDataSource;
+import net.sf.jasperreports.json.data.JsonDataSource;
+import net.sf.jasperreports.json.query.JsonQLDataSource;
 
 import org.apache.commons.lang.StringEscapeUtils;
 
